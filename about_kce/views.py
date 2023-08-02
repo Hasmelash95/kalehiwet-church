98cabdd135652e3697791fab4549ba328a8842d7
Hasmelash95 <103432143+Hasmelash95@users.noreply.github.com>
2023-08-02 14:09:42 +0000

    Add member profile to the about page

--- about_kce/views.py
+++ about_kce/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, HttpResponse
 from django.views import generic, View
+from .models import Profile
 
 # Create your views here.
 
 def home_page(request):
     
     return render(request, 'index.html', None)
+
+class MemberProfile(generic.ListView):
+
+    model = Profile
+    template_name = 'about.html'
+    context_object_name = 'profile'
