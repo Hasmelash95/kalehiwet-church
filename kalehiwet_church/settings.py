43a9ae337ceafde81acdf24606a128f9ad7b9707
Hasmelash95 <hermonasmelash@yahoo.com>
2023-08-05 14:23:38 +0000

    Install the dependencies

--- kalehiwet_church/settings.py
+++ kalehiwet_church/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '8000-hasmelash95-kalehiwetch-wav8l8opyt6.ws-eu102.gitpod.io'
+]
 
 
 # Application definition
